4d2fddc210bf6ceb805d72cb15691737e1201afa
TacoTheDank <SkytkRSfan3895@gmail.com>
2021-06-08 22:45:48 -0400

    Test-fix issue number nine... number nine, number nine, number nine...

--- app/src/main/java/taco/scoop/ui/SettingsActivity.kt
+++ app/src/main/java/taco/scoop/ui/SettingsActivity.kt
@@ -5,9 +5,12 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import taco.scoop.R
 import taco.scoop.databinding.ActivitySettingsBinding
+import taco.scoop.util.readLogsPermissionGranted
+import taco.scoop.util.runReadLogsGrantShell
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,6 +37,20 @@ class SettingsActivity : AppCompatActivity() {
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             addPreferencesFromResource(R.xml.preferences)
+
+            val rootStatusPref = findPreference<Preference>("prefKey_root_status")
+            rootStatusPref?.setOnPreferenceClickListener {
+                runReadLogsGrantShell()
+                true
+            }
+
+            val permissionStatusPref = findPreference<Preference>("prefKey_permission_status")
+            permissionStatusPref?.summary =
+                if (requireContext().readLogsPermissionGranted()) {
+                    getString(R.string.settings_permission_status_summary_true)
+                } else {
+                    getString(R.string.settings_permission_status_summary_false)
+                }
         }
     }
 }
